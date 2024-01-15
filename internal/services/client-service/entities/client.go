facccdbeac56bcebca1393d777f53265923d7bed
vnniciusg <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2024-01-15 17:22:40 -0300

    feat : add client entity

---
+++ internal/services/client-service/entities/client.go
@@ -0,0 +1,17 @@
+package entities
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Client struct {
+	Id             uuid.UUID        `json:"id"`
+	Name           string           `json:"name"`
+	BirthDate      time.Time        `json:"birth_date"`
+	Sexo           string           `json:"sexo"`
+	HealthProblems []HealthProblems `json:"health_problems"`
+	CreatedAt      time.Time        `json:"created_at"`
+	UpdatedAt      time.Time        `json:"updated_at"`
+}
