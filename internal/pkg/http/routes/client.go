e93d652e11fd362a47984aef07a45e213c1a6520
vnniciusg <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2024-01-18 08:40:15 -0300

    feat : add client router

---
+++ internal/pkg/http/routes/client.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"database/sql"
+
+	"github.com/gin-gonic/gin"
+	"github.com/vnniciusg/backend-developer-challenge/internal/services/client-service/di"
+)
+
+func ClientRoutes(conn *sql.DB, r *gin.RouterGroup) {
+
+	clientHandler := di.ConfigClientDI(conn)
+
+	clientGroup := r.Group("/clients")
+
+	clientGroup.POST("/", clientHandler.CreateClient)
+
+}
