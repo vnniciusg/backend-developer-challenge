b5f3dfc3c1a254b3e27d1c7fa1f69d4a887d0a06
vnniciusg <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2024-01-18 14:45:32 -0300

    fix : insert health problem sql

--- internal/services/client-service/respositories/healthproblemrepository/sqlstatements/insertHealthProblem.go
+++ internal/services/client-service/respositories/healthproblemrepository/sqlstatements/insertHealthProblem.go
@@ -1,8 +1,8 @@
-package sqlstatments
+package sqlstatements
 
 const InsertHealthProblem = `
 	INSERT INTO tb_health_problems 
-	(client_id, name, grau) 
-	VALUES ($1, $2, $3) 
+	(client_id, name, grau , created_at, updated_at) 
+	VALUES ($1, $2, $3 , $4, $5) 
 	RETURNING id , client_id, name, grau, created_at, updated_at
 `
