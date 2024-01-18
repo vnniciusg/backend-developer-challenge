baa2e9d069b76c26a32b4d338b513bde5bdb04a4
vnniciusg <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2024-01-18 14:43:12 -0300

    fix : create client request DTO

--- internal/services/client-service/dto/request/createClientRequestDTO.go
+++ internal/services/client-service/dto/request/createClientRequestDTO.go
@@ -1,10 +1,8 @@
 package request
 
-import "time"
-
 type CreateClientRequestDTO struct {
 	Name           string                           `json:"name"`
-	BirthDate      time.Time                        `json:"birth_date"`
+	BirthDate      string                           `json:"birth_date"`
 	Sexo           string                           `json:"sexo"`
 	HealthProblems []*CreateHealthProblemRequestDTO `json:"health_problems"`
 }
--- internal/services/client-service/dto/request/createHealthProblemRequestDTO.go
+++ internal/services/client-service/dto/request/createHealthProblemRequestDTO.go
@@ -1,9 +1,6 @@
 package request
 
-import "github.com/google/uuid"
-
 type CreateHealthProblemRequestDTO struct {
-	ClientId uuid.UUID `json:"client_id"`
-	Name     string    `json:"name"`
-	Grau     int       `json:"grau"`
+	Name string `json:"name"`
+	Grau int    `json:"grau"`
 }
