4f12e74ad1101a8785bc000159248d51bc090494
vnniciusg <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2024-01-22 13:16:38 -0300

    refactor : create client repository

--- internal/services/client-service/respositories/clientrespository/clientrepositoryimpl/createClient.go
+++ internal/services/client-service/respositories/clientrespository/clientrepositoryimpl/createClient.go
@@ -9,24 +9,12 @@ import (
 	"github.com/vnniciusg/backend-developer-challenge/internal/pkg/date"
 	"github.com/vnniciusg/backend-developer-challenge/internal/pkg/persistence/utils"
 	"github.com/vnniciusg/backend-developer-challenge/internal/services/client-service/dto/request"
-	"github.com/vnniciusg/backend-developer-challenge/internal/services/client-service/entities"
 	"github.com/vnniciusg/backend-developer-challenge/internal/services/client-service/respositories/sqlstatements/clientsqlstatements"
-	"github.com/vnniciusg/backend-developer-challenge/internal/services/client-service/respositories/sqlstatements/healthproblemsqlstatements"
 )
 
-func (cr *ClientRepository) CreateClient(client *request.CreateClientRequestDTO) (*entities.Client, error) {
-
-	var createdClient *entities.Client
+func (cr *ClientRepository) CreateClient(client *request.CreateClientRequestDTO) error {
 
 	err := utils.WithTransaction(cr.DB, func(tx *sql.Tx) error {
-
-		created_at, err := date.GetCurrentTime()
-
-		if err != nil {
-			return err
-		}
-
-		updated_at := created_at
 
 		birthDate, err := date.ParseDate(client.BirthDate)
 
@@ -34,49 +22,34 @@ func (cr *ClientRepository) CreateClient(client *request.CreateClientRequestDTO)
 			return errors.New("Falha ao converter data de nascimento")
 		}
 
-		createdClient, err = insertClient(tx, client.Name, birthDate, client.Sexo, created_at, updated_at)
+		err = insertClient(tx, client.Name, birthDate, client.Sexo)
 
 		if err != nil {
 			return err
-		}
-
-		for _, healthProblem := range client.HealthProblems {
-			err = insertHealthProblem(tx, createdClient, healthProblem, created_at, updated_at)
-			if err != nil {
-				return err
-			}
-
 		}
 
 		return nil
 
 	})
 
-	return createdClient, err
+	return err
 }
 
-func insertClient(tx *sql.Tx, name string, birthDate time.Time, sexo string, createdAt time.Time, updatedAt time.Time) (*entities.Client, error) {
-	row := tx.QueryRow(clientsqlstatements.InsertClient, name, birthDate, sexo, createdAt, updatedAt)
+func insertClient(tx *sql.Tx, name string, birthDate time.Time, sexo string) error {
 
-	createdClient := &entities.Client{}
+	created_at, err := date.GetCurrentTime()
 
-	if err := row.Scan(&createdClient.Id, &createdClient.Name, &createdClient.BirthDate, &createdClient.Sexo, &createdClient.CreatedAt, &createdClient.UpdatedAt); err != nil {
-		return nil, err
-	}
-
-	return createdClient, nil
-}
-
-func insertHealthProblem(tx *sql.Tx, client *entities.Client, healthProblem *request.CreateHealthProblemRequestDTO, createdAt time.Time, updatedAt time.Time) error {
-	healthProblemResponse := &entities.HealthProblems{}
-
-	row := tx.QueryRow(healthproblemsqlstatements.InsertHealthProblem, client.Id, healthProblem.Name, healthProblem.Grau, createdAt, updatedAt)
-
-	if err := row.Scan(&healthProblemResponse.Id, &healthProblemResponse.ClientId, &healthProblemResponse.Name, &healthProblemResponse.Grau, &healthProblemResponse.CreatedAt, &healthProblemResponse.UpdatedAt); err != nil {
+	if err != nil {
 		return err
 	}
 
-	client.HealthProblems = append(client.HealthProblems, healthProblemResponse)
+	updated_at := created_at
+
+	_, err = tx.Exec(clientsqlstatements.InsertClient, name, birthDate, sexo, created_at, updated_at)
+
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
--- internal/services/client-service/usecase/clientusecase/createClient.go
+++ internal/services/client-service/usecase/clientusecase/createClient.go
@@ -2,15 +2,14 @@ package clientusecase
 
 import (
 	"github.com/vnniciusg/backend-developer-challenge/internal/services/client-service/dto/request"
-	"github.com/vnniciusg/backend-developer-challenge/internal/services/client-service/entities"
 )
 
-func (cuc *ClientUseCase) CreateClient(client *request.CreateClientRequestDTO) (*entities.Client, error) {
-	clientResponse, err := cuc.clientRepository.CreateClient(client)
+func (cuc *ClientUseCase) CreateClient(client *request.CreateClientRequestDTO) error {
+	err := cuc.clientRepository.CreateClient(client)
 
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return clientResponse, nil
+	return nil
 }
