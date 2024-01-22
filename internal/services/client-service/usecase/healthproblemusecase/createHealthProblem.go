d8e65f1922e02be152a74b02229519557515e311
vnniciusg <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2024-01-22 13:35:51 -0300

    refactor : change response

--- internal/services/client-service/usecase/healthproblemusecase/createHealthProblem.go
+++ internal/services/client-service/usecase/healthproblemusecase/createHealthProblem.go
@@ -5,7 +5,7 @@ import (
 	"github.com/vnniciusg/backend-developer-challenge/internal/services/client-service/dto/request"
 )
 
-func (hpuc *HealthProblemUseCase) CreateHealthProblem(clientId uuid.UUID, healthProblem *request.CreateHealthProblemRequestDTO) error {
+func (hpuc *HealthProblemUseCase) CreateHealthProblem(clientId uuid.UUID, healthProblem []*request.CreateHealthProblemRequestDTO) error {
 
 	err := hpuc.healthProblemRepository.CreateHealthProblem(clientId, healthProblem)
 
