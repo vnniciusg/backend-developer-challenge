caf14f221dbcaebd35db1f8385c4a422f7028b57
vnniciusg <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2024-01-22 13:15:55 -0300

    fix : change clientId type

--- internal/services/client-service/entities/healthProblems.go
+++ internal/services/client-service/entities/healthProblems.go
@@ -9,7 +9,7 @@ import (
 type HealthProblems struct {
 	Id        uuid.UUID `json:"id"`
 	Name      string    `json:"name"`
-	ClientId  string    `json:"client_id"`
+	ClientId  uuid.UUID `json:"client_id"`
 	Grau      int       `json:"grau"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
