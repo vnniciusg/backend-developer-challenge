2269ebca565fca8481c3cb97be297c872a879725
vnniciusg <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2024-01-26 08:36:23 -0300

    refactor : entities

--- internal/services/client-service/entities/client.go
+++ internal/services/client-service/entities/client.go
@@ -1,6 +1,7 @@
 package entities
 
 import (
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
@@ -13,4 +14,28 @@ type Client struct {
 	Sexo      string    `json:"sexo"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func NewClient(name string, birthDate time.Time, sexo string) *Client {
+	return &Client{
+		Name:      name,
+		BirthDate: birthDate,
+		Sexo:      sexo,
+	}
+}
+
+func ValidateClient(client *Client) error {
+	if client.Name == "" {
+		return errors.New("Nome do cliente não pode ser vazio")
+	}
+
+	if client.Sexo == "" {
+		return errors.New("Sexo do cliente não pode ser vazio")
+	}
+
+	if client.BirthDate.IsZero() {
+		return errors.New("Data de nascimento do cliente não pode ser vazio")
+	}
+
+	return nil
 }
--- internal/services/client-service/entities/healthProblems.go
+++ internal/services/client-service/entities/healthProblems.go
@@ -1,6 +1,7 @@
 package entities
 
 import (
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
@@ -13,4 +14,31 @@ type HealthProblems struct {
 	Grau      int       `json:"grau"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func NewHealthProblems(name string, clientId uuid.UUID, grau int) *HealthProblems {
+	return &HealthProblems{
+		Name:     name,
+		ClientId: clientId,
+		Grau:     grau,
+	}
+}
+
+func ValidateHealthProblems(healthProblems *HealthProblems) error {
+	if healthProblems.Name == "" {
+		return errors.New("Nome do problema de saúde não pode ser vazio")
+	}
+
+	if healthProblems.ClientId == uuid.Nil {
+		return errors.New("Id do cliente não pode ser vazio")
+	}
+
+	if healthProblems.Grau != 1 && healthProblems.Grau != 2 {
+		if healthProblems.Grau == 0 {
+			return errors.New("Grau do problema de saúde não pode ser vazio")
+		}
+		return errors.New("Grau do problema de saúde deve ser 1 ou 2")
+	}
+
+	return nil
 }
