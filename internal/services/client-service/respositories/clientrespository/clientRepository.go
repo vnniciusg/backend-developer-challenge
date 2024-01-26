24b7248eae04500235df16e0bd0c7f0df8a931c5
vnniciusg <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2024-01-26 08:52:29 -0300

    refactor : client repositorie

--- internal/services/client-service/respositories/clientrespository/clientRepository.go
+++ internal/services/client-service/respositories/clientrespository/clientRepository.go
@@ -2,14 +2,14 @@ package clientrespository
 
 import (
 	"github.com/google/uuid"
-	"github.com/vnniciusg/backend-developer-challenge/internal/services/client-service/dto/request"
 	"github.com/vnniciusg/backend-developer-challenge/internal/services/client-service/dto/response"
+	"github.com/vnniciusg/backend-developer-challenge/internal/services/client-service/entities"
 )
 
 type ClientRepository interface {
 	FindAllClients() ([]*response.GetClientResponseDTO, error)
 	FindClientById(id uuid.UUID) (*response.GetClientResponseDTO, error)
-	CreateClient(client *request.CreateClientRequestDTO) error
-	UpdateClient(id uuid.UUID, client *request.UpdateClientRequestDTO) error
+	CreateClient(client *entities.Client) error
+	UpdateClient(client *entities.Client) error
 	// TopTenClientsWithHighestHealthRisk() ([]*entities.Client, error)
 }
